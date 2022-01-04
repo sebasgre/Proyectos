88b985d0900a6e77a8f42624b3610253097fa2d9
Sebastian Greiner <sebastiang300.sg@gmail.com>
2022-01-04 16:03:13 -0400

    Estamos probando comentarios mediante una extension en vscode

--- Backend/Java/src/main/java/Programacion3/Animacion/Main/Main.java
+++ Backend/Java/src/main/java/Programacion3/Animacion/Main/Main.java
@@ -4,6 +4,12 @@ import Programacion3.Animacion.Gui.FrameAnimacion;
 
 public class Main {
     public static void main(String[] args) {
+        //* Este es un pequeño comentario
+        //? Esto es una prueba
+        /**
+         * 
+         * 
+         */
         FrameAnimacion win = new FrameAnimacion();
         win.setVisible(true);
     }
